0810959e10b0645a284c306b6e26095325450131
Riley McDowell <mcdori02@luther.edu>
2015-07-07 20:47:59 -0500

    Adding a testing directory before I implement an RBF NN.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, Command
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys
+        import subprocess 
+        errno = subprocess.call([sys.executable, 'runtest.py'])
+        raise SystemExit(errno)
+
+setup( name='genomic_neuralnet'
+     , version='0.0.1'
+     , description='Evaluation of various genomic selection and whole genome prediction methods on public datasets'
+     , author='Riley McDowell'
+     , author_email='mcdori02_at_luther_dot_edu'
+     , url='https://github.com/rileymcdowell/genomic-neuralnet'
+     , packages=find_packages()
+     , cmdclass = { 'test': PyTest }
+     )
+
+
---
+++ tests/test_test.py
@@ -0,0 +1,2 @@
+def test_tests_test():
+    assert True
