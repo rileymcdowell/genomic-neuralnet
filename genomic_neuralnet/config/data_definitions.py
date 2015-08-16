7ee25a9bac01a32fd1015e22475f2bf5165b59de
Riley McDowell <mcdori02@luther.edu>
2015-08-16 14:35:45 -0500

    Adding crossa data for grain yield predictions.

--- genomic_neuralnet/config/data_definitions.py
+++ genomic_neuralnet/config/data_definitions.py
@@ -10,10 +10,16 @@ _data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
 #_pheno_path = os.path.join(_data_dir, 'arabidopsis', 'phenotypes.csv')
 #pheno = pd.DataFrame.from_csv(_pheno_path, index_col=None, sep=',')
 
-# Crossa Maize Data
-TRAIT_NAME = 'ww_flm' # Female flowering time, well-watered environment.
-_marker_path = os.path.join(_data_dir, 'crossa', 'maize', 'genotypes.csv')
-markers = pd.DataFrame.from_csv(_marker_path, index_col=None, sep=',', header=None)
-_pheno_path = os.path.join(_data_dir, 'crossa', 'maize', 'phenotypes.csv')
-pheno = pd.DataFrame.from_csv(_pheno_path, index_col=None, sep=',')
+# Crossa Maize Flowering Data
+#TRAIT_NAME = 'ww_flf' # Female flowering time, well-watered environment.
+#_marker_path = os.path.join(_data_dir, 'crossa', 'maize', 'flowering', 'genotypes.csv')
+#markers = pd.DataFrame.from_csv(_marker_path, index_col=None, sep=',', header=None)
+#_pheno_path = os.path.join(_data_dir, 'crossa', 'maize', 'flowering', 'phenotypes.csv')
+#pheno = pd.DataFrame.from_csv(_pheno_path, index_col=None, sep=',')
 
+# Crossa Maize Yield Data
+TRAIT_NAME = 'ss_yld' # Grain yield, severe stress environment.
+_marker_path = os.path.join(_data_dir, 'crossa', 'maize', 'grain_yield', 'genotypes.csv')
+markers = pd.DataFrame.from_csv(_marker_path, index_col=None, sep=',', header=None)
+_pheno_path = os.path.join(_data_dir, 'crossa', 'maize', 'grain_yield', 'phenotypes.csv')
+pheno = pd.DataFrame.from_csv(_pheno_path, index_col=None, sep=',')
