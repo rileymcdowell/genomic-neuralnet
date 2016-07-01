b7a20207668f246d510caa68ad1e7e18632383cf
Riley McDowell <mcdori02@luther.edu>
2016-06-30 19:24:08 -0500

    Training script seems functional.
    The optimize all script is running. Renamed
    a few files to be compatible with the optimizing
    bash script. It can be run with
    `bash optimize_all.sh &> optimization.log`.

--- genomic_neuralnet/analyses/optimize_all.sh
+++ genomic_neuralnet/analyses/optimize_all.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
+source $(which virtualenvwrapper.sh)
 workon genomic_sel
 
 # Array of species.
-declare -a SPECIES=(arabidopsis wheat pig maize loblolly)
+#declare -a SPECIES=(arabidopsis wheat pig maize loblolly)
+
+# Array of species.
+declare -a SPECIES=(arabidopsis wheat maize) # Start easy.
+
+echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+echo "Beginning Optimization. Time is: $(date)"
+echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
 
 # Loop over every optimization function for every species and trait.
 # This will populate csv files which store the output of the optimizations.
-for file in $( ls optimize* ) ; do
+for file in $( ls optimize*.py ) ; do
     for species in ${SPECIES[@]} ; do
         for trait in $(python $file --species $species --list ) ; do
-            python $f --species $species --trait $trait
+            echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+            echo "INFO: About to train ${file} - ${species} - ${trait}"
+            echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+
+            echo $file | grep 'nn.py' > /dev/null
+            if [ $? -eq 0 ] ; then
+                # Train neural nets on GPU.
+                python $file --species $species --trait $trait --gpu
+            else 
+                # Train others in normal mode.
+                python $file --species $species --trait $trait
+            fi
+
+            echo '####################################################'
+            echo "Training File Completed. Time is $(date)"
+            echo '####################################################'
         done
     done
 done
