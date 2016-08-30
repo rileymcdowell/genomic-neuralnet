c7941340336b3fe584dd192583c088eb1f1f972e
Riley McDowell <mcdori02@luther.edu>
2016-08-29 20:57:12 -0500

    Reduce result broker message timeout

--- genomic_neuralnet/common/celeryconfig.py
+++ genomic_neuralnet/common/celeryconfig.py
@@ -1,10 +1,11 @@
 
 # Wait up to 15 minutes for each iteration.
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} # Seconds = 1 hour.
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 60*60} # 60*60 Seconds = 1 hour.
 # Do not pre-fetch work.
 CELERYD_PREFETCH_MULTIPLIER = 1 
 # Do not ack messages until work is completed.
 CELERY_ACKS_LATE = True
 # Stop warning me about PICKLE.
 CELERY_ACCEPT_CONTENT = ['pickle']
-
+# Clear out finished results after 30 minutes.
+CELERY_TASK_RESULT_EXPIRES = 60*30
